a81383e4651e15e1118bd2b4ef69d1e19fcf92d6
JenMora <jeniffermourah73@gmail.com>
2023-05-18 08:24:41 +0000

    Scripted commit

---
+++ sizeof.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+int main(void)
+{
+int size = sizeof(int);
+
+int array[5];
+int arraySize = sizeof(array);
+int valueSize = sizeof(array[0]);
+return (0);
+}
+
